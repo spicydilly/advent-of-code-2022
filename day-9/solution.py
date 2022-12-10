d69ffee24cb5afd590bf627ee6a2ff629154fb66
Dylon <dylon.condon@chrobinson.com>
2022-12-10 20:09:16 +0000

    add day 9, add devcontainer

--- day-9/solution.py
+++ day-9/solution.py
@@ -8,16 +8,42 @@ Usage   : call with 'solution.py --input <input file name>'
 """
 
 import argparse
-from ast import parse
+import numpy as np
+
+
+class Knot():
+    """
+    Class that defines a knot
+    """
+
+    def __init__(self, pos_x, pos_y):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+
+    def __sub__(self, knot):
+        """
+        Subtracting a knot, provides the distance
+        """
+        return Knot(self.pos_x - knot.pos_x, self.pos_y - knot.pos_y)
+
+    def normalize(self):
+        """
+        Returns normalized pos_x y
+        """
+        return np.linalg.norm([self.pos_x, self.pos_y])
 
 
 class Solution():
+    """
+    Class that builds the solution
+    """
 
     def __init__(self):
-        self.closest_tail_path = [[0, 0]]
-        # [x, y]
-        self.head_location = [0, 0]
-        self.tail_location = [0, 0]
+        self.last_knot_trail = []
+        self.rope = []
+        self.number_of_knots = 2
+        self.result_part_one = 0
+        self.result_part_two = 0
 
     def get_arguments(self):
         """
@@ -26,111 +52,67 @@ class Solution():
         parser = argparse.ArgumentParser()
         parser.add_argument("--input", type=str, required=True,
                             help="Input file")
-        parser.add_argument("--knots", type=str, required=True,
-                            help="Number of knots")
-        self.args = parser.parse_args()
-        self.process_file(self.args.input, self.args.knots)
-        self.result_part_one = self.number_of_locations_visited_once()
-        self.result_part_two = self.number_of_locations_visited_once(False)
+        args = parser.parse_args()
+        self.process_file(args.input, 2)
+        self.result_part_one = self.number_of_locations_visited_by_last_knot()
+        self.process_file(args.input, 10)
+        self.result_part_two = self.number_of_locations_visited_by_last_knot()
 
-    def process_file(self, input_file, knots):
+    def process_file(self, input_file, number_of_knots):
         """
         Reads the input file, returns directory as dictionary
         """
-        head_location = self.head_location
-        tail_locations = [self.tail_location * knots]
-        with open(input_file, 'r') as f:
+        self.number_of_knots = number_of_knots
+        self.rope = [
+            Knot(0, 0)
+            for i in range(number_of_knots)
+        ]
+        self.last_knot_trail = {
+            (
+                self.rope[number_of_knots-1].pos_x,
+                self.rope[number_of_knots-1].pos_y
+            )
+        }
+        with open(input_file, 'r', encoding="utf-8") as f:
             all_instructions = f.read().splitlines()
             for instruction in all_instructions:
                 if instruction:
-                    head_location, tail_locations = self.determine_movement(
-                        head_location, tail_locations, instruction.split())
-        return
+                    self.rope = self.determine_movement(
+                        self.rope, instruction.split())
+        return self.rope
 
-    def determine_movement(self, head_location, tail_locations, instruction):
+    def determine_movement(self, rope, instruction):
         """
         Determines the move to take
         """
-        move = instruction[0]
-        amount = instruction[1]
-        if move == "R":
-            head_location, tail_locations = self.move_head(
-                0, 1, head_location, tail_locations, amount)
-        elif move == "L":
-            head_location, tail_locations = self.move_head(
-                0, -1, head_location, tail_locations, amount)
-        elif move == "U":
-            head_location, tail_locations = self.move_head(
-                1, 1, head_location, tail_locations, amount)
-        elif move == "D":
-            head_location, tail_locations = self.move_head(
-                1, -1, head_location, tail_locations, amount)
-        return head_location, tail_locations
+        direction = instruction[0]
+        amount = int(instruction[1])
+        for i in range(1, int(amount)+1):
+            match direction:
+                case "R":
+                    rope[0].pos_x += 1
+                case "L":
+                    rope[0].pos_x -= 1
+                case "U":
+                    rope[0].pos_y += 1
+                case "D":
+                    rope[0].pos_y -= 1
+            for knot in range(self.number_of_knots-1):
+                if (rope[knot] - rope[knot+1]).normalize() >= 2:
+                    rope[knot+1].pos_x += (rope[knot+1].pos_x != rope[knot].pos_x) * \
+                        np.sign(rope[knot].pos_x - rope[knot+1].pos_x)
+                    rope[knot+1].pos_y += (rope[knot+1].pos_y != rope[knot].pos_y) * \
+                        np.sign(rope[knot].pos_y - rope[knot+1].pos_y)
+                    if knot+1 == self.number_of_knots-1:
+                        self.last_knot_trail.add(
+                            (rope[knot+1].pos_x, rope[knot+1].pos_y))
+        return rope
 
-    def move_head(self, direction, step, head_location, tail_locations, amount):
-        """
-        Moves the head by amount, and checks for tail updates each step
-
-        direction: 0 for x, 1 for y
-        """
-        for each in range(int(amount)):
-            old_head_location = head_location.copy()
-            head_location[direction] += step
-            tail_locations = self.determine_tail_movement(
-                head_location, tail_locations, old_head_location)
-            # keep copy of visit
-            self.closest_tail_path += [tail_locations.copy()]
-        return head_location, tail_locations
-
-    def determine_tail_movement(self, head_location, tail_locations, old_head_location):
-        """
-        Determines where the tail should move after the head has moved
-        """
-        for knot in range(tail_locations):
-            # check for overlap
-            if head_location == tail_locations[knot]:
-                return tail_locations[knot]
-            x_difference = head_location[0] - tail_locations[knot][0]
-            y_difference = head_location[1] - tail_locations[knot][1]
-            # check if head x is more than 1 step away than tail x
-            if (x_difference > 1) or (x_difference < -1):
-                # check if head y is same as tail y
-                if head_location[1] == tail_locations[knot][1]:
-                    # move in direction of x
-                    tail_locations[knot] = [
-                        tail_locations[knot][0] + (x_difference/2),
-                        tail_locations[knot][1]
-                    ]
-                else:
-                    # move in direction of y
-                    tail_locations[knot] = old_head_location
-            # check if head y is more than 1 step away than tail y
-            elif (y_difference > 1) or (y_difference < -1):
-                # check if head y is same as tail y
-                if head_location[1] == tail_locations[knot][1]:
-                    # move in direction of x
-                    tail_locations[knot] = [
-                        tail_locations[knot][0],
-                        tail_locations[knot][1] + (y_difference/2)
-                    ]
-                else:
-                    # move in direction of y
-                    tail_locations[knot] = old_head_location
-        return tail_locations
-
-    def number_of_locations_visited_once(self, closest=True):
+    def number_of_locations_visited_by_last_knot(self):
         """
         Returns number of locations that were visited once
         """
-        unique_visits = []
-        tail_locations = self.furthest_tail_path
-        if closest:
-            tail_locations = self.closest_tail_path
-        for visit in tail_locations:
-            if visit in unique_visits:
-                continue
-            unique_visits += [visit]
-        return len(unique_visits)
+        return len(self.last_knot_trail)
 
 
 if __name__ == "__main__":
---
+++ day-9/test_solution.py
@@ -0,0 +1,31 @@
+from solution import Solution
+
+
+def test_solution_example_part_one():
+    solution = Solution()
+    solution.process_file("example_input.txt", 2)
+    assert solution.number_of_locations_visited_by_last_knot() == 13
+
+
+def test_solution_example_part_two():
+    solution = Solution()
+    solution.process_file("example_input.txt", 10)
+    assert solution.number_of_locations_visited_by_last_knot() == 1
+
+
+def test_solution_example_two_part_two():
+    solution = Solution()
+    solution.process_file("example_input2.txt", 10)
+    assert solution.number_of_locations_visited_by_last_knot() == 36
+
+
+def test_solution_real_input_part_one():
+    solution = Solution()
+    solution.process_file("input.txt", 2)
+    assert solution.number_of_locations_visited_by_last_knot() == 5960
+
+
+def test_solution_real_input_part_two():
+    solution = Solution()
+    solution.process_file("input.txt", 10)
+    assert solution.number_of_locations_visited_by_last_knot() == 2327
