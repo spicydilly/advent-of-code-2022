b4cc3a00f6cadbbc5555d17f7f1573a8bd65ebfb
Dylon <dyloncondon@gmail.com>
2022-12-12 03:26:39 +0000

    fix port

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@ app="aoc2022"
 docker rm $(docker stop $(docker ps -a -q --filter ancestor=${app} --format="{{.ID}}"))
 
 docker build -t ${app} .
-docker run -d -p 5000:5000 ${app}
-#docker run -it --rm -p 5000:5000 ${app} sh
+docker run -d -p 5557:5000 ${app}
+#docker run -it --rm -p 5557:5000 ${app} sh
